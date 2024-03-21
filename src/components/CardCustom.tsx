86593203f01b3c765d23b3c913626c403889d143
ElishaMarks <ironbasedalloy@gmail.com>
2024-03-21 09:04:13 +0200

    At least done

---
+++ src/components/CardCustom.tsx
@@ -0,0 +1,21 @@
+import { Card } from 'antd';
+import { IProduct } from '../layouts/ContentCustom';
+const { Meta } = Card;
+
+interface IProductProps{
+    product:  IProduct
+}
+
+const CardCustom = (props: IProductProps) => {
+    const {product}= props;
+    return(
+        <Card
+            hoverable
+            style={{ width: 240 }}
+            cover={<img alt={product.title} src={product.thumbnail} />}
+        >
+            <Meta title = {product.title} description={product.description} />
+        </Card>
+    );
+}
+export default CardCustom;
